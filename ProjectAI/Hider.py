764e7ed451abb5b6140f7c7eaff28b49439f502e
Dongz125 <hoangkhai3122004@gmail.com>
2024-04-04 08:42:51 +0700

    sửa hider.py từ tụt bùi đến hoàn thiện :))

--- ProjectAI/Hider.py
+++ ProjectAI/Hider.py
@@ -1,15 +1,15 @@
-﻿import random
+﻿import math
+import random
 from Board import Board
 
 class Hider:
     def __init__(self, x, y):
-        self.position = (x, y)  # Starting position
+        self.position = (x, y) # Starting position
         self.announce_position = (-1, 0)
         
-    def check_valid_move(self, board, x, y, direction):       
-        # check valid movement for hider
-        if 0 <= x < board.m and 0 <= y < board.n:
-            if board.map_with_objects[y][x] == 0:
+    def check_valid_move(self, board, x, y, direction): # return true if valid movement     
+        if 0 <= x < board.m and 0 <= y < board.n: # if position is in range of map
+            if board.map_with_objects[y][x] == 0: # if next position is empty
                 if direction == "left" or direction == "right" or direction == "up" or direction == "down":
                     return True
                 else:
@@ -59,17 +59,16 @@ class Hider:
             
         # Check if movement is valid
         if self.check_valid_move(board, x, y, direction):
-            self.position = (x, y) # if valid, save position
-            
-        return self.position
+            return (x, y) # if valid, return position   
+        else: return self.position # else, return current position (no move)
 
     def announce(self, board): # return position of announcement     
         rand_announce = random.randint(1, 49) # random position
         x_hider, y_hider = self.position
         
         dem = 1;
-        for i in range(max(y_hider - 3, 0), min(y_hider + 3, board.n - 1)):
-            for j in range(max(x_hider - 3, 0), min(x_hider + 3, board.m - 1)):
+        for i in range(max(y_hider - 3, 0), min(y_hider + 3, board.n - 1) + 1):
+            for j in range(max(x_hider - 3, 0), min(x_hider + 3, board.m - 1) + 1):
                 if dem == rand_announce: # correct position to announce
                     self.announce_position = j, i # save announce position
                     return (j, i)
@@ -79,12 +78,14 @@ class Hider:
         x_hider, y_hider = self.position
         x_seeker, y_seeker = -1, 0
         
-        for i in range(max(y_hider - 2, 0), min(y_hider + 2, board.n - 1)):
-            for j in range(max(x_hider - 2, 0), min(x_hider + 2, board.m - 1)):
+        for i in range(max(y_hider - 2, 0), min(y_hider + 2, board.n - 1) + 1):
+            for j in range(max(x_hider - 2, 0), min(x_hider + 2, board.m - 1) + 1):
                 if board.map_with_objects[i][j] == 3:
                     x_seeker = j
                     y_seeker = i
-                    j = board.m, i = board.n # end loop when fought
+                    # end loop when fought
+                    j = board.m + 1
+                    i = board.n + 1 
                     
         if x_seeker != -1: # saw seeker
             x_vector, y_vector = x_seeker - x_hider, y_seeker - y_hider # cal vector to have direction
@@ -118,23 +119,36 @@ class Hider:
 
         return (-1, 0) # not see
     
-    def movement_strategy(self, board):
+    def movement_strategy(self, board): # function to move, no return
         x_seeker, y_seeker = self.look_around(board) # look around before moving
         x_moved, y_moved = self.position
+        all_directions = ["up", "down", "left", "right", "up_left", "up_right", "down_left", "down_right"]
         
-        while (x_moved, y_moved) == self.position:
-            if x_seeker == -1: # not fought seeker
-                # move randomly
-                random_direction = random.randint(1, 8)
-            
-                if random_direction == 1: direction = "up"
-                elif random_direction == 2: direction = "down"
-                elif random_direction == 3: direction = "left"
-                elif random_direction == 4: direction = "right"
-                elif random_direction == 5: direction = "up_left"
-                elif random_direction == 6: direction = "up_right"
-                elif random_direction == 7: direction = "down_left"
-                elif random_direction == 8: direction = "down_right"
-            
-                x_moved, y_moved = self.move(direction, board)
-            else: # seeker fought
+        if x_seeker == -1: # not fought seeker
+            # move randomly
+            selected_directions = []
+
+            while (x_moved, y_moved) == self.position and len(selected_directions) < len(all_directions): # choose a direction which not similar to all previous one
+                remaining_directions = [d for d in all_directions if d not in selected_directions]
+                direction = random.choice(remaining_directions)
+    
+                selected_directions.append(direction)
+                    
+                x_moved, y_moved = self.move(direction, board) # move test, if fail, return current position
+                
+            self.position = (x_moved, y_moved) # modify position (if cannot move, standing)
+
+        else: # seeker fought
+            distance_to_seeker = (self.position[0] - x_seeker) ** 2 + (self.position[1] - y_seeker) ** 2
+            valid_position = []
+                
+            for direction in all_directions: # find all possible way
+                (xi, yi) = self.move(direction, board)
+                    
+                if (xi, yi) != self.position and (xi - x_seeker) ** 2 + (xi - y_seeker) ** 2 >= distance_to_seeker: # if movable and increase or keep distance
+                    valid_position.append((xi, yi))
+                        
+            if len(valid_position) > 0: # if there is any possible position, move based on max distance
+                max_distance_position = max(valid_position, key=lambda pos: (pos[0] - x_seeker) ** 2 + (pos[1] - y_seeker) ** 2)
+                self.position = max_distance_position
+            # else, no move, just standing
